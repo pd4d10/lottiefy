660773edd02ac39ef203d2c7b86ac9fa62dc32e0
Rongjian Zhang <pd4d10@gmail.com>
2018-03-08 22:33:45 +0800

    Add rollup and typescript config

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import typescript from 'rollup-plugin-typescript2'
+
+export default [
+  {
+    input: './src/index.ts',
+    plugins: [typescript()],
+    output: {
+      file: 'dist/lottie-cocos.js',
+      format: 'iife',
+    },
+    // moduleName: 'lottie-cocos',
+  },
+]
