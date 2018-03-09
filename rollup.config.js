beab372cbceb92444680426adbe9af2b0fe3dab3
Rongjian Zhang <pd4d10@gmail.com>
2018-03-09 19:49:44 +0800

    specify cache folder of rollup ts plugin

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,11 @@ import typescript from 'rollup-plugin-typescript2'
 export default [
   {
     input: './src/index.ts',
-    plugins: [typescript()],
+    plugins: [
+      typescript({
+        cacheRoot: '/tmp/rollup-ts-cache',
+      }),
+    ],
     output: {
       file: 'dist/lottie-cocos.js',
       format: 'iife',
