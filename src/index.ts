91382670e99786941bcf0f9dfb0ea1093024f04c
Rongjian Zhang <pd4d10@gmail.com>
2018-03-11 21:55:41 +0800

    fix position and shape

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,17 @@ import { traverse } from './traverse'
 export default function lottie(data: any, g: any) {
   const layers: { [id: string]: cc.Layer | cc.Sprite | cc.DrawNode } = {}
 
-  const container = new cc.LayerColor(cc.color(0, 0, 255, 50), 100, 100)
+  const container = new cc.LayerColor(cc.color(0, 0, 255, 100), 1080, 1920)
   // container.setContentSize(100, 100)
   // container.ignoreAnchorPointForPosition(false)
   // container.setAnchorPoint(0.5, 0.5)
 
   // container.setPosition(0, cc.winSize.height)
-  container.setPosition(400, 2500)
+  // container.setPosition(cc.winSize.width / 2, cc.winSize.height / 2)
+  container.setScale(0.4)
+  // container.setPosition(500, 500)
   // window.g = globalLayer
   // container.setScale(cc.winSize.height / 1920)
-  container.setScale(0.5)
   const containerId = 'xxxxx'
   layers[containerId] = container
   g.addChild(container)
@@ -35,7 +36,8 @@ export default function lottie(data: any, g: any) {
       y3: number
     ) {
       // console.log(arguments)
-      // const node = new cc.DrawNode()
+      const parentNode = this.getNode(parentId)
+      const node = new cc.DrawNode()
       // setTimeout(() => {
       //   node.drawDots(
       //     [cc.p(x0, y0), cc.p(x1, y1), cc.p(x2, y2), cc.p(x3, y3)],
@@ -53,24 +55,28 @@ export default function lottie(data: any, g: any) {
       //   10,
       //   cc.color('#ff0')
       // )
-      // layers[id] = node
-      // this.addChild(id, parentId)
+      node.drawSegment(cc.p(x0, parentNode.height - y0), cc.p(x3, parentNode.height - y3), 10)
+      layers[id] = node
+      this.addChild(id, parentId)
       // console.log(node)
     },
-    createLayer(id) {
-      layers[id] = new cc.LayerColor(cc.color(0, 0, 0, 0))
+    createLayer(id, width, height) {
+      // console.log(id)
+      layers[id] = new cc.LayerColor(cc.color(255, 255, 0, 40), width, height)
     },
     createSprite(id, name) {
       // layers[id] = new cc.Sprite(cc.spriteFrameCache.getSpriteFrame(name))
       layers[id] = new cc.Sprite(cc.spriteFrameCache.getSpriteFrame(name))
     },
     setPosition(id, parentId, x, y) {
+      // console.log(id, x, layers[parentId].height - y, layers[id])
       layers[id].setPosition(x, layers[parentId].height - y)
     },
     setContentSize(id, width, height) {
       layers[id].setContentSize(width, height)
     },
     setAnchorPoint(id, x, y) {
+      // console.log(arguments)
       layers[id].ignoreAnchorPointForPosition(false)
       layers[id].setAnchorPoint(x, y)
     },
@@ -80,6 +86,7 @@ export default function lottie(data: any, g: any) {
     },
     addChild(id, parentId, localZOrder) {
       // console.log(arguments)
+      // layers[id].ignoreAnchorPointForPosition(false)
       layers[parentId].addChild(layers[id], localZOrder)
     },
     getNode(id) {
--- src/traverse.ts
+++ src/traverse.ts
@@ -29,7 +29,7 @@ interface Options {
     x3: number,
     y3: number
   ): any
-  createLayer(id: string): any
+  createLayer(id: string, width?: number, height?: number): any
   createSprite(id: string, name: string): any
   setPosition(id: string, parentId: string, x: number, y: number): any
   setAnchorPoint(id: string, x: number, y: number): any
@@ -38,9 +38,6 @@ interface Options {
   addChild(id: string, parentId: string, localZOrder?: number): any
   getNode(id: string): cc.Node
 }
-
-const getTime = (time: number) => 4 * time
-
 // type Asset =
 //   | {
 //       h?: Height1
@@ -57,6 +54,8 @@ const getTime = (time: number) => 4 * time
 //     }
 
 export function traverse(data: any, containerId: string, options: Options) {
+  const getTime = (time: number) => time / data.fr
+
   let assets: {
     [id: string]: any
   }
@@ -73,14 +72,23 @@ export function traverse(data: any, containerId: string, options: Options) {
     return assets[id]
   }
 
-  function _traverseShape(shape: any, parentId: string) {
+  function _traverseShape(shape: any, parentId: string, node?: cc.DrawNode) {
     switch (shape.ty) {
       case Shape.group: {
+        const node = new cc.DrawNode()
+        options.getNode(parentId).addChild(node)
         for (let item of shape.it) {
-          _traverseShape(item, parentId)
+          _traverseShape(item, parentId, node)
+        }
+      }
+      case Shape.stroke: {
+        if (node) {
+          node.setColor(cc.color(shape.c.k.map((x: number) => x * 255)))
+          node.width = shape.w.k
         }
       }
       case Shape.shape: {
+        // const id = cid || v4()
         const id = v4()
         const c = (x: any) => {
           const { i, o, v } = x
@@ -112,7 +120,7 @@ export function traverse(data: any, containerId: string, options: Options) {
           // )
         }
         if (shape.ks) {
-          console.log(shape.ks.k)
+          // console.log(shape.ks.k)
           if (Array.isArray(shape.ks.k)) {
             c(shape.ks.k[0].s[0])
           } else {
@@ -142,141 +150,186 @@ export function traverse(data: any, containerId: string, options: Options) {
     ellipse = 'el',
     group = 'gr',
     shape = 'sh',
+    transform = 'tr',
+    stroke = 'st',
+  }
+
+  enum Effect {
+    slider = 0,
+    angle = 1,
+    color = 2,
+    point = 3,
+    checkbox = 4,
+    group = 5,
+    dropDown = 7,
+  }
+
+  function _traverseEffect(effect: any, parentId: string) {
+    switch (effect.ty) {
+      case Effect.group: {
+        for (let item of effect.ef) {
+          _traverseEffect(item, parentId)
+        }
+      }
+      case Effect.dropDown: {
+      }
+    }
+  }
+
+  function _traverseLayer(layer: any, parentId: string, options: Options) {
+    // console.log(layer.nm)
+    // options.createLayer(id)
+
+    switch (layer.ty) {
+      case Layer.shape: {
+        const id = v4()
+        options.createLayer(id, 0, 0)
+        options.setPosition(id, parentId, layer.ks.p.k[0], layer.ks.p.k[1])
+        options.addChild(id, parentId)
+        for (let shape of layer.shapes) {
+          _traverseShape(shape, id)
+        }
+        break
+      }
+      case Layer.solid: {
+        break
+      }
+      case Layer.image: {
+        const id = layer.refId
+        const asset = getAsset(id)
+        if (!asset) break
+        options.createSprite(id, asset.p)
+        options.setContentSize(id, asset.w, asset.h)
+        options.getNode(id).ignoreAnchorPointForPosition(false)
+        options.setAnchorPoint(id, layer.ks.a.k[0] / asset.w, layer.ks.a.k[1] / asset.h)
+        options.getNode(id).setPosition(layer.ks.p.k[0], layer.ks.p.k[1])
+        options.addChild(id, parentId)
+        break
+      }
+      case Layer.precomp: {
+        const id = layer.refId
+        const asset = getAsset(id)
+
+        if (!asset) break
+
+        // precomp
+        options.createLayer(id, layer.w, layer.h)
+
+        // size
+        // options.setContentSize(id, layer.w || asset.w, layer.h || asset.h)
+
+        if (layer.ks.s && layer.ks.s.k) {
+          const [x, y] = layer.ks.s.k
+          if (typeof x === 'number' && typeof y === 'number') {
+          }
+        }
+
+        if (layer.ks.o) {
+        }
+
+        const parseK = (k: any[]) => {
+          // console.log(k)
+          return k.reduceRight(
+            (result, item) => {
+              const { s, e, to, ti, t } = item
+              if (s) {
+                result.arr.unshift({ s, e, to, ti, t: getTime(result.nextTime - t) })
+              } else {
+                // result.allTime = t
+              }
+              result.nextTime = t
+              return result
+            },
+            {
+              nextTime: null,
+              // allTime: null,
+              arr: [],
+            }
+          )
+        }
+
+        // anchor
+        if (layer.ks.a && layer.ks.a.k) {
+          const [x, y] = layer.ks.a.k
+          if (typeof x === 'number' && typeof y === 'number') {
+            let { w = 10000, h = 10000 } = layer // FIXME:
+            options.setAnchorPoint(id, x / w, 1 - y / h)
+          }
+        }
+
+        // position
+        if (layer.ks.p) {
+          if (typeof layer.ks.p.k[0] === 'number') {
+            var [x, y] = layer.ks.p.k
+            options.setPosition(id, parentId, x, y)
+          } else if (layer.ks.p.k.length) {
+            let a: any = []
+            options.setPosition(id, parentId, layer.ks.p.k[0].s[0], layer.ks.p.k[0].s[1])
+            const parentHeight = options.getNode(parentId).height
+            parseK(layer.ks.p.k).arr.forEach((x: any) => {
+              a.push(
+                cc.bezierTo(x.t, [
+                  cc.p(x.s[0], parentHeight - x.s[1]),
+                  cc.p(x.ti[0], parentHeight - x.ti[1]),
+                  cc.p(x.to[0], parentHeight - x.to[1]),
+                  cc.p(x.e[0], parentHeight - x.e[1]),
+                ])
+              )
+            })
+
+            options.getNode(id).runAction(cc.sequence(a))
+          }
+        }
+
+        // rotation
+        if (layer.ks.r && layer.ks.r.k) {
+          if (typeof layer.ks.r.k[0] === 'number') {
+          } else {
+            let a: any = []
+            parseK(layer.ks.r.k).arr.forEach((x: any) => {
+              a.push(cc.rotateTo(0, x.s[0]))
+              a.push(cc.rotateTo(x.t, x.e[0]))
+            })
+            options.getNode(id).runAction(cc.sequence(a))
+          }
+        }
+
+        // scale
+        if (layer.ks.s) {
+          if (typeof layer.ks.s.k[0] === 'number') {
+          } else {
+            let a: any = []
+            parseK(layer.ks.s.k).arr.forEach((x: any) => {
+              a.push(cc.scaleTo(0, x.s[0] / 100, x.s[1] / 100))
+              a.push(cc.scaleTo(x.t, x.e[0] / 100, x.e[1] / 100))
+            })
+
+            options.getNode(id).runAction(cc.sequence(a))
+          }
+        }
+
+        // effects
+        if (layer.ef) {
+          for (let item of layer.ef) {
+            _traverseEffect(item, parentId)
+          }
+        }
+
+        if (asset.layers) {
+          for (let layer of asset.layers) {
+            _traverseLayer(layer, id, options)
+          }
+        }
+
+        options.addChild(id, parentId)
+        break
+      }
+    }
   }
 
   function _traverse(item: any, parentId: string, options: Options) {
-    if (!item.layers) {
-      return
-    }
-    const itemId = item.id
-
     for (let layer of item.layers) {
-      console.log(layer.nm)
-
-      const id = v4()
-      // options.createLayer(id)
-
-      switch (layer.ty) {
-        case Layer.shape: {
-          // if (layer.shapes) {
-          for (let shape of layer.shapes) {
-            _traverseShape(shape, parentId)
-          }
-          // } else {
-          // }
-          break
-        }
-        case Layer.solid: {
-          break
-        }
-        case Layer.image: {
-          const id = layer.refId
-          const asset = getAsset(id)
-          if (!asset) break
-          options.createSprite(id, asset.p)
-          options.setContentSize(id, asset.w, asset.h)
-          options.addChild(id, parentId)
-          break
-        }
-        case Layer.precomp: {
-          const id = layer.refId
-          const asset = getAsset(id)
-
-          if (!asset) break
-
-          if (asset.layers) {
-            // precomp
-            options.createLayer(id)
-          }
-
-          // size
-          // options.setContentSize(id, layer.w || asset.w, layer.h || asset.h)
-
-          if (layer.ks.s && layer.ks.s.k) {
-            const [x, y] = layer.ks.s.k
-            if (typeof x === 'number' && typeof y === 'number') {
-            }
-          }
-
-          if (layer.ks.o) {
-          }
-
-          const parseK = (k: any[]) => {
-            console.log(k)
-            return k.reduceRight(
-              (result, item) => {
-                const { s, e, t } = item
-                if (s) {
-                  result.arr.unshift({ s, e, t: getTime((result.nextTime - t) / result.allTime) })
-                } else {
-                  result.allTime = t
-                }
-                result.nextTime = t
-                return result
-              },
-              { nextTime: null, allTime: null, arr: [] }
-            )
-          }
-
-          // anchor
-          if (layer.ks.a && layer.ks.a.k) {
-            const [x, y] = layer.ks.a.k
-            if (typeof x === 'number' && typeof y === 'number') {
-              let { w = 10000, h = 10000 } = layer // FIXME:
-              options.setAnchorPoint(id, x / w, 1 - y / h)
-            }
-          }
-
-          // position
-          if (layer.ks.p) {
-            if (typeof layer.ks.p.k[0] === 'number') {
-              var [x, y] = layer.ks.p.k
-              options.setPosition(id, parentId, x, y)
-            } else if (layer.ks.p.k.length) {
-              let a: any = []
-              parseK(layer.ks.p.k).arr.forEach((x: any) => {
-                a.push(cc.moveTo(0, x.s[0], options.getNode(parentId).height - x.s[1]))
-                a.push(cc.moveTo(x.t, x.e[0], options.getNode(parentId).height - x.e[1]))
-              })
-              options.getNode(parentId).runAction(cc.sequence(a))
-            }
-          }
-
-          // rotation
-          if (layer.ks.r && layer.ks.r.k) {
-            if (typeof layer.ks.r.k[0] === 'number') {
-            } else {
-              let a: any = []
-              parseK(layer.ks.r.k).arr.forEach((x: any) => {
-                a.push(cc.rotateTo(0, x.s[0]))
-                a.push(cc.rotateTo(x.t, x.e[0]))
-              })
-              options.getNode(parentId).runAction(cc.sequence(a))
-            }
-          }
-
-          // scale
-          if (layer.ks.s) {
-            if (typeof layer.ks.s.k[0] === 'number') {
-            } else {
-              let a: any = []
-              parseK(layer.ks.s.k).arr.forEach((x: any) => {
-                a.push(cc.scaleTo(0, x.s[0] / 100, x.s[1] / 100))
-                a.push(cc.scaleTo(x.t, x.e[0] / 100, x.e[1] / 100))
-              })
-
-              options.getNode(parentId).runAction(cc.sequence(a))
-            }
-          }
-
-          if (asset.layers) {
-            _traverse(asset, id, options)
-          }
-
-          options.addChild(id, parentId)
-          break
-        }
-      }
+      _traverseLayer(layer, parentId, options)
     }
   }
 
