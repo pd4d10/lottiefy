f1594ce822233243060c145630df1a78d6accb5f
Rongjian Zhang <pd4d10@gmail.com>
2018-05-05 17:31:23 +0800

    Restore Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = {
+  watch: true,
+  devtool: 'inline-source-map',
+  output: {
+    path: path.resolve('./dist'),
+    filename: 'lottie-cocos2dx.js',
+    library: 'lottie',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [{ test: /\.ts$/, use: 'ts-loader' }],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  mode: 'development',
+}
