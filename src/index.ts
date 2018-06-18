744c63d815b234f5237a3fbeb8e31ce7c029902c
Rongjian Zhang <pd4d10@gmail.com>
2018-06-18 17:34:47 +0800

    Remove unused code

--- src/index.ts
+++ src/index.ts
@@ -101,134 +101,6 @@ export default class LottieRenderer {
     }
   }
 
-  // TODO: Effects
-  // _traverseEffect(data: any, parentId: string, node: cc.DrawNode) {
-  //   switch (data.ty) {
-  //     case Effect.group: {
-  //       if (data.mn === 'ADBE Gaussian Blur 2') {
-  //       }
-  //       for (let item of data.ef) {
-  //         this._traverseEffect(item, parentId, node)
-  //       }
-  //     }
-  //     case Effect.dropDown: {
-  //     }
-  //   }
-  // }
-
-  // TODO: Shapes
-  // private _traverseShape(
-  //   data: any,
-  //   parentId: string,
-  //   parentWidth: number,
-  //   parentHeight: number,
-  //   id?: string,
-  //   payload?: any,
-  // ) {
-  //   switch (data.ty) {
-  //     case Shape.group: {
-  //       const id = this.generateId()
-  //       const payload: {
-  //         id: string
-  //         stroke: { width: number; color: Color }
-  //         shape: BezierPoints[]
-  //         shapeKeyframes: ShapeAnimationData[]
-  //         ellipse: any
-  //         transform: any
-  //       } = {
-  //         id,
-  //         stroke: { width: 0, color: [0, 0, 0, 0] },
-  //         shape: [],
-  //         shapeKeyframes: [],
-  //         ellipse: null,
-  //         transform: null,
-  //       }
-  //       for (let item of data.it) {
-  //         this._traverseShape(item, parentId, parentWidth, parentHeight, id, payload)
-  //       }
-  //       this.actions.createShape(id, payload.stroke.width)
-  //       this.actions.appendChild(id, parentId)
-  //       payload.shape.forEach(item => {
-  //         this.actions.drawCubicBezier(id, item, payload.stroke.width, payload.stroke.color)
-  //       })
-  //       payload.shapeKeyframes.forEach(item => {
-  //         this.actions.drawCubicBezierAnimation(
-  //           id,
-  //           item,
-  //           payload.stroke.width,
-  //           payload.stroke.color,
-  //         )
-  //       })
-  //       // if (payload.ellipse) {
-  //       //   let [x, y] = payload.transform.p.k
-  //       //   const center = { x, y: -y }
-  //       //   const [rx, ry] = payload.ellipse.s.k
-  //       //   this.actions.drawEllipse(
-  //       //     id,
-  //       //     center,
-  //       //     rx,
-  //       //     ry,
-  //       //     0,
-  //       //     100,
-  //       //     true,
-  //       //     payload.stroke.width,
-  //       //     payload.stroke.color,
-  //       //     payload.fill.color,
-  //       //   )
-  //       // }
-
-  //       break
-  //       // options.curveAnimate(id, 10, { r: 255, g: 0, b: 0, a: 255 }, d.data)
-  //     }
-  //     case Shape.stroke: {
-  //       if (id) {
-  //         payload.stroke = {
-  //           width: data.w.k,
-  //           color: convertColor(data.c.k),
-  //         }
-  //       }
-  //       break
-  //     }
-  //     case Shape.shape: {
-  //       if (id && payload && data.ks) {
-  //         if (Array.isArray(data.ks.k)) {
-  //           const keyframes = data.ks.k as ShapeKeyframe[]
-  //           let d: ShapeAnimationData[] = []
-  //           keyframes.slice(0, -1).forEach((keyframe, i) => {
-  //             d.push({
-  //               delay: i === 0 ? this._getTime(keyframe.t) : 0,
-  //               duration: this._getTime(keyframes[i + 1].t - keyframe.t),
-  //               start: this._convertBezierShapeToPoints(keyframe.s[0], parentHeight),
-  //               end: this._convertBezierShapeToPoints(keyframe.e[0], parentHeight),
-  //             })
-  //           })
-  //           payload.shapeKeyframes = d
-  //         } else {
-  //           const x = data.ks.k as BezierShape
-  //           payload.shape = this._convertBezierShapeToPoints(x, parentHeight)
-  //         }
-  //       }
-  //       break
-  //     }
-  //     case Shape.transform:
-  //       payload.transform = data
-  //       break
-  //     case Shape.ellipse:
-  //       payload.ellipse = data
-  //       break
-  //     case Shape.fill:
-  //       payload.fill = {
-  //         color: convertColor(data.c.k),
-  //         opacity: data.o.k,
-  //       }
-  //       break
-  //     case Shape.merge:
-  //       break
-  //     default:
-  //     // console.log(data.ty)
-  //   }
-  // }
-
   private _convertBezierShapeToPoints(
     bezierShape: BezierShape,
     parentHeight: number,
@@ -423,28 +295,7 @@ export default class LottieRenderer {
     if (!this.layerFilter(layer)) return
 
     switch (layer.ty) {
-      case LayerType.shape: {
-        // const id = this.generateId()
-        // same width and height as parent
-        // options.createLayer(id, parentWidth, parentHeight)
-        // this.actions.createPrecomp(id, { width: 0, height: 0 })
-        // this._applyTransform(
-        //   layer,
-        //   id,
-        //   parentId,
-        //   parentWidth,
-        //   parentHeight,
-        //   parentWidth,
-        //   parentHeight,
-        //   startFrame,
-        // )
-        // this.actions.appendChild(id, parentId)
-        // TODO: Shape parser is not ready
-        // for (let shape of layer.shapes) {
-        //   this._traverseShape(shape, id, parentWidth, parentHeight)
-        // }
-        break
-      }
+      case LayerType.shape:
       case LayerType.solid:
         break
       case LayerType.image: {
@@ -500,13 +351,6 @@ export default class LottieRenderer {
           parentHeight,
           startFrame,
         )
-
-        // TODO: Effects is not ready
-        // if (layer.ef) {
-        //   for (let item of layer.ef) {
-        //     this._traverseEffect(item, parentId)
-        //   }
-        // }
 
         const asset = this.assets[layer.refId] as PrecompAsset
         if (asset && asset.layers) {
